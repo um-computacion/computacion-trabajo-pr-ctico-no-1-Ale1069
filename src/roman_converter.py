ac72142b63a85446600e1719f71d3b6b7529e3c4
Ale1069 <ala.carrizo@alumno.um.edu.ar>
2025-04-08 19:06:53 -0300

    Implementando la función decimal_to_roman

---
+++ src/roman_converter.py
@@ -0,0 +1,21 @@
+def decimal_to_roman(number):
+    val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+    ]
+    syms = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+    ]
+    roman_num = ''
+    i = 0
+    while number > 0:
+        for _ in range(number // val[i]):
+            roman_num += syms[i]
+            number -= val[i]
+        i += 1
+    return roman_num
